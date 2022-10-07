a8511fbdc2658d34d369fb91ceb3000e7da599a9
kwzr <kwzr.poyo@gmail.com>
2022-10-07 21:10:21 +0900

    Add Practice4

---
+++ Practice4/Practice4/UI/IllustViewModel.swift
@@ -0,0 +1,19 @@
+import Firebase
+
+class IllustViewModel {
+    @Published var illusts: [Illust] = []
+
+    private let repository: IllustRepository
+
+    init(repository: IllustRepository) {
+        self.repository = repository
+    }
+
+    func fetchIllusts() async {
+        do {
+            (illusts, _) = try await repository.fetchIllusts()
+        } catch {
+            print(error)
+        }
+    }
+}
